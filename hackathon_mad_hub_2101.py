ba58eb03071a0d6ff027a1597060a797e76e7aa2
Borja Serra <francisco.de.borja.serra.planelles@ing.com>
2021-01-28 15:30:48 +0100

    type check

--- hackathon_mad_hub_2101.py
+++ hackathon_mad_hub_2101.py
@@ -2,11 +2,19 @@ import requests
 
 
 def post_score(team, score):
+	if not isinstance(score, list):
+		print("'Score' should be a list of numbers!")
+		return
+	if not score:
+		print("'Score' is empty!")
+		return
+	if not isinstance(team, str):
+		print("'Team' should be a string!")
+		return
+
     score_url = "https://hackathon-ing-mad-hub-2101.herokuapp.com/score"
     score_query = "?team={}&score={}"
 
     r1 = requests.post(score_url + score_query.format(team, score))
 
     print(r1.text)
-
-
