53060f738b4f2c8f517fc25044edd99cbbc20f58
Borja Serra <francisco.de.borja.serra.planelles@ing.com>
2021-01-28 16:29:23 +0100

    mean change

--- hackathon_mad_hub_2101.py
+++ hackathon_mad_hub_2101.py
@@ -2,19 +2,21 @@ import requests
 
 
 def post_score(team, score):
-	if not isinstance(score, list):
-		print("'Score' should be a list of numbers!")
-		return
-	if not score:
-		print("'Score' is empty!")
-		return
-	if not isinstance(team, str):
-		print("'Team' should be a string!")
-		return
+    if not isinstance(score, list):
+        print("'Score' should be a list of numbers!")
+        return
+    if not score:
+        print("'Score' is empty!")
+        return
+    if not isinstance(team, str):
+        print("'Team' should be a string!")
+        return
+
+    score_avg = sum(score)/len(score)
 
     score_url = "https://hackathon-ing-mad-hub-2101.herokuapp.com/score"
     score_query = "?team={}&score={}"
 
-    r1 = requests.post(score_url + score_query.format(team, score))
+    r1 = requests.post(score_url + score_query.format(team, [score_avg]))
 
     print(r1.text)
