e3b2973f63abf00e3c2e4284ea80b39a87151559
Borja Serra <francisco.de.borja.serra.planelles@ing.com>
2021-01-29 09:17:53 +0100

    Last changes

--- hackathon_mad_hub_2101.py
+++ hackathon_mad_hub_2101.py
@@ -1,7 +1,17 @@
 import requests
 
 
-def post_score(team, score):
+def post_score(team, metrics):
+    ave_reward_list, successful, last_position_episodes = metrics
+
+    EPISODE_THRESHOLD = 10000
+
+    if len(last_position_episodes) < EPISODE_THRESHOLD:
+        print(f"Not enough episodes. Minimum required {EPISODE_THRESHOLD}")
+        return
+
+    score = last_position_episodes[:EPISODE_THRESHOLD]
+
     if not isinstance(score, list):
         print("'Score' should be a list of numbers!")
         return
@@ -13,10 +23,12 @@ def post_score(team, score):
         return
 
     score_avg = sum(score)/len(score)
+    success_avg = sum(successful)/len(successful)
+    reward_avg = sum(ave_reward_list)/sum(ave_reward_list)
 
     score_url = "https://hackathon-ing-mad-hub-2101.herokuapp.com/score"
-    score_query = "?team={}&score={}"
+    score_query = "?team={}&score={}&successful={}&reward={}"
 
-    r1 = requests.post(score_url + score_query.format(team, [score_avg]))
+    r1 = requests.post(score_url + score_query.format(team, [score_avg], [success_avg], [reward_avg]))
 
     print(r1.text)
